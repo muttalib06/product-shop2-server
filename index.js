38759a8691c5f5f5ffb839df990785140d67f7ce
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2025-10-31 07:52:33 +0600

    added update api by id

--- index.js
+++ index.js
@@ -28,19 +28,44 @@ async function run() {
     await client.connect();
 
     // create database and collection reference;
+    // for product
     const database = client.db("productDB");
     const productCollection = database.collection("products");
+    // for user
+    const userDatabase = client.db("userDB");
+    const userCollection = userDatabase.collection("users");
     // read all products
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find();
       const products = await cursor.toArray();
       res.send(products);
     });
-    //create data(insert)
+    // read users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    // read use by id;
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    //create data(insert) for product
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
       console.log("New Product", newProduct);
       const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // create data for users;
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
 
@@ -48,6 +73,21 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // update user ;
+    app.put("/users/:id", async (req, res) => {
+      const updatedUser = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
     // send a ping to confirm successful connection
